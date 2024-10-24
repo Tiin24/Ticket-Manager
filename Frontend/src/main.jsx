68641244067661f47d08479d3a6fea56fa0c50c7
Tiin24 <86517179+Tiin24@users.noreply.github.com>
2024-10-24 03:31:54 -0300

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,9 +7,6 @@ import { ThemeProvider } from "./context/ThemeContext";
 
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
